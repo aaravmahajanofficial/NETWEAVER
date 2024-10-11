77eaa3bb8bdd03f3669c0822bd48be0ff7892c9d
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-10-12 03:35:51 +0530

    Add custom typography

---
+++ app/src/main/java/com/example/netweaver/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.example.netweaver.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.netweaver.R
+
+val segoeUiFontFamily = FontFamily(
+    Font(R.font.segoeui, FontWeight.Normal),
+    Font(R.font.seguisb, FontWeight.SemiBold)
+)
--- app/src/main/java/com/example/netweaver/ui/theme/Type.kt
+++ app/src/main/java/com/example/netweaver/ui/theme/Type.kt
@@ -2,33 +2,80 @@ package com.example.netweaver.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    // Display styles
+    displayLarge = TextStyle(
+        fontFamily = segoeUiFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    // Heading styles
+    headlineLarge = TextStyle(
+        fontFamily = segoeUiFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    // Subheading
+    headlineMedium = TextStyle(
+        fontFamily = segoeUiFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+    // Button text
+    labelLarge = TextStyle(
+        fontFamily = segoeUiFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    // Body text
+    bodyLarge = TextStyle(
+        fontFamily = segoeUiFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    // Body text 1
+    bodyMedium = TextStyle(
+        fontFamily = segoeUiFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    // Body text 2
+    bodySmall = TextStyle(
+        fontFamily = segoeUiFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
+    ),
+    // Body text 3
+    titleMedium = TextStyle(
+        fontFamily = segoeUiFontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
+    ),
+    // Navigation text
+    titleSmall = TextStyle(
+        fontFamily = segoeUiFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
     )
-    */
 )
