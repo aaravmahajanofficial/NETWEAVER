025a89114d637e7c6882f90d999e7e22a98ec33c
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-10-13 03:38:41 +0530

    Implement CommonTopBar.kt

--- app/src/main/java/com/example/netweaver/ui/components/CommonTopBar.kt
+++ app/src/main/java/com/example/netweaver/ui/components/CommonTopBar.kt
@@ -1,2 +1,29 @@
 package com.example.netweaver.ui.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommonTopBar(modifier: Modifier = Modifier, title: String, showBack: Boolean = true) {
+    TopAppBar(
+        title = {
+            Text(title)
+        },
+        navigationIcon = {
+            if (showBack) {
+                IconButton(onClick = {}) {
+                    Icon(Icons.AutoMirrored.Default.ArrowBack, contentDescription = null)
+                }
+            } else null
+        }
+    )
+
+}
