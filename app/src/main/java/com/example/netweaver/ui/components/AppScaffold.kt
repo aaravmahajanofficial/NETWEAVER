abefa47edbe567e7e242c6d79be024211ed5556b
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-10-13 02:27:25 +0530

    Add UI components

---
+++ app/src/main/java/com/example/netweaver/ui/components/AppScaffold.kt
@@ -0,0 +1,26 @@
+package com.example.netweaver.ui.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AppScaffold(
+    modifier: Modifier = Modifier,
+    topBar: @Composable () -> Unit,
+    bottomBar: @Composable () -> Unit,
+    floatingActionButton: @Composable () -> Unit,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        floatingActionButton = floatingActionButton
+    ) { paddingValues ->
+        content(paddingValues)
+
+    }
+
+}
