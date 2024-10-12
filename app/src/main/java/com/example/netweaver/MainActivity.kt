72a0300f95ab2139eec63be916527f4a6e12ee72
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-10-13 03:41:05 +0530

    Add HomeScreen.kt

--- app/src/main/java/com/example/netweaver/MainActivity.kt
+++ app/src/main/java/com/example/netweaver/MainActivity.kt
@@ -4,15 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
+import com.example.netweaver.ui.features.home.HomeScreen
 import com.example.netweaver.ui.theme.NETWEAVERTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -33,29 +27,8 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             NETWEAVERTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NETWEAVERTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/netweaver/ui/features/home/HomeScreen.kt
@@ -0,0 +1,59 @@
+package com.example.netweaver.ui.features.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.netweaver.ui.components.AppScaffold
+import com.example.netweaver.ui.components.CommonTopBar
+import com.example.netweaver.ui.theme.AppPadding
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+
+    AppScaffold(
+        topBar = {
+            CommonTopBar(title = "Home", showBack = false)
+        }
+
+    ) { innerPadding ->
+
+        HomeContent(
+            paddingValues = PaddingValues(
+                top = innerPadding.calculateTopPadding(),
+                bottom = innerPadding.calculateBottomPadding(),
+                start = AppPadding.medium,
+                end = AppPadding.medium
+            )
+        )
+
+    }
+}
+
+@Composable
+private fun HomeContent(
+    paddingValues: PaddingValues,
+) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+
+    ) {
+
+    }
+
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun Test() {
+//    NETWEAVERTheme {
+//        HomeScreen()
+//    }
+//}
