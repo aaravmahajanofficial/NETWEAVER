0cbe0e7aefeae570937df2340caf998d71b83f5b
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-10-14 04:04:24 +0530

    Update netweaverMediumGray color

--- app/src/main/java/com/example/netweaver/ui/theme/Color.kt
+++ app/src/main/java/com/example/netweaver/ui/theme/Color.kt
@@ -6,4 +6,4 @@ import androidx.compose.ui.graphics.Color
 val netweaverBlue = Color(0xFF0a66c2)
 val netweaverLightGray = Color(0xfff4f2ee)
 val netweaverDarkGray = Color(0xFF1D2226)
-val netweaverMediumGray = Color(0xBF000000)
+val netweaverMediumGray = Color(0xE6000000)
