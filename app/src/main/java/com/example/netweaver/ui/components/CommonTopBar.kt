23ce093e89a7d7cabcbd649311fe083a8a955bbd
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-10-13 06:04:39 +0530

    Update CommonTopBar.kt with dynamic content and actions

--- app/src/main/java/com/example/netweaver/ui/components/CommonTopBar.kt
+++ app/src/main/java/com/example/netweaver/ui/components/CommonTopBar.kt
@@ -1,29 +1,90 @@
 package com.example.netweaver.ui.components
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@SuppressLint("RememberReturnType")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CommonTopBar(modifier: Modifier = Modifier, title: String, showBack: Boolean = true) {
-    TopAppBar(
+fun CommonTopBar(
+    title: String? = null,
+    showBack: Boolean = true,
+    actions: @Composable RowScope.() -> Unit = {},
+    leading: @Composable (() -> Unit) = {}
+) {
+
+    var searchQuery by remember { mutableStateOf("") }
+
+    CenterAlignedTopAppBar(
         title = {
-            Text(title)
+            if (title != null) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.displayLarge
+                )
+            } else {
+
+                OutlinedTextField(
+                    value = searchQuery,
+                    onValueChange = {
+                        searchQuery = it
+                    },
+                    placeholder = {
+                        Text(
+                            "Search",
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.onSecondary
+                        )
+                    },
+                    leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = MaterialTheme.colorScheme.secondary,
+                        unfocusedContainerColor = MaterialTheme.colorScheme.secondary,
+                    ),
+                    singleLine = true,
+                    maxLines = 1,
+                    modifier = Modifier
+                        .height(48.dp)
+                        .width(286.dp)
+                )
+
+
+            }
+
         },
         navigationIcon = {
             if (showBack) {
                 IconButton(onClick = {}) {
                     Icon(Icons.AutoMirrored.Default.ArrowBack, contentDescription = null)
                 }
-            } else null
-        }
+            } else {
+                leading()
+            }
+        },
+
+        actions = {
+            actions()
+        },
     )
 
 }
--- app/src/main/java/com/example/netweaver/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/example/netweaver/ui/features/home/HomeScreen.kt
@@ -1,15 +1,23 @@
 package com.example.netweaver.ui.features.home
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.netweaver.R
 import com.example.netweaver.ui.components.AppScaffold
 import com.example.netweaver.ui.components.CommonTopBar
 import com.example.netweaver.ui.theme.AppPadding
 
+@SuppressLint("RememberReturnType")
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier
@@ -17,7 +25,22 @@ fun HomeScreen(
 
     AppScaffold(
         topBar = {
-            CommonTopBar(title = "Home", showBack = false)
+            CommonTopBar(
+                showBack = false,
+                actions = {},
+                leading = {
+                    Box(
+                        modifier = Modifier
+                            .background(
+                                shape = CircleShape,
+                                color = colorResource(R.color.black)
+                            )
+                            .size(32.dp)
+                            .padding(start = AppPadding.medium)
+                    ) {
+
+                    }
+                })
         },
         content = { innerPadding ->
             HomeContent(
