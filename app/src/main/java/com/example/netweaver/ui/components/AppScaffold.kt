38ba4391b9f0347fce33dc3462c176863e12020f
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-10-18 01:37:04 +0530

    Refactor AppScaffold.kt

--- app/src/main/java/com/example/netweaver/ui/components/AppScaffold.kt
+++ app/src/main/java/com/example/netweaver/ui/components/AppScaffold.kt
@@ -1,23 +1,108 @@
 package com.example.netweaver.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.netweaver.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppScaffold(
     content: @Composable (PaddingValues) -> Unit,
-    modifier: Modifier = Modifier,
-    topBar: @Composable () -> Unit = {},
-    bottomBar: @Composable () -> Unit = {},
-    floatingActionButton: @Composable () -> Unit = {}
 ) {
+
+    val navItems = listOf(
+        BottomItem(
+            icon = painterResource(R.drawable.home),
+            selectedIcon = painterResource(R.drawable.selected_home),
+            label = "Home"
+        ),
+        BottomItem(
+            icon = painterResource(R.drawable.my_network),
+            selectedIcon = painterResource(R.drawable.selected_my_network),
+            label = "My Network"
+        ),
+        BottomItem(
+            icon = painterResource(R.drawable.create),
+            selectedIcon = painterResource(R.drawable.create),
+            label = "Post"
+        ),
+        BottomItem(
+            icon = painterResource(R.drawable.notifications),
+            selectedIcon = painterResource(R.drawable.selected_notifications),
+            label = "Notifications"
+        ),
+        BottomItem(
+            icon = painterResource(R.drawable.jobs),
+            selectedIcon = painterResource(R.drawable.selected_jobs),
+            label = "Jobs"
+        )
+    )
+
+    var selectedItem by remember { mutableStateOf(navItems[0]) }
+
+    val scrollBehavior =
+        TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
     Scaffold(
-        modifier = modifier,
-        topBar = topBar,
-        bottomBar = bottomBar,
-        floatingActionButton = floatingActionButton
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CommonTopBar(
+                scrollBehavior = scrollBehavior,
+                showBack = false,
+                actions = {
+
+                    Icon(
+                        painter = painterResource(R.drawable.message),
+                        contentDescription = "Message",
+                        modifier = Modifier.size(24.dp),
+                        tint = MaterialTheme.colorScheme.tertiary
+                    )
+
+                },
+                avatar = {
+                    Box(
+                        modifier = Modifier
+                            .background(
+                                shape = CircleShape,
+                                color = colorResource(R.color.black)
+                            )
+                            .size(32.dp)
+                    ) {
+
+                    }
+                })
+        },
+        bottomBar = {
+            CommonBottomBar(
+                actions = {
+                    navItems.forEach { item ->
+                        CustomBarItem(item.copy(isSelected = item == selectedItem), onClick = {
+                            selectedItem = item
+                        })
+                    }
+                }
+            )
+        },
+        floatingActionButton = {}
     ) { paddingValues ->
         content(paddingValues)
 
--- app/src/main/java/com/example/netweaver/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/example/netweaver/ui/features/home/HomeScreen.kt
@@ -1,117 +1,23 @@
 package com.example.netweaver.ui.features.home
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import com.example.netweaver.R
 import com.example.netweaver.ui.components.AppScaffold
-import com.example.netweaver.ui.components.BottomItem
-import com.example.netweaver.ui.components.CommonBottomBar
-import com.example.netweaver.ui.components.CommonTopBar
-import com.example.netweaver.ui.components.CustomBarItem
 import com.example.netweaver.ui.features.home.components.PostCard
 
 @OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("RememberReturnType")
 @Composable
 fun HomeScreen(
 ) {
 
-    val scrollBehavior =
-        TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
-
-    val navItems = listOf(
-        BottomItem(
-            icon = painterResource(R.drawable.home),
-            selectedIcon = painterResource(R.drawable.selected_home),
-            label = "Home"
-        ),
-        BottomItem(
-            icon = painterResource(R.drawable.my_network),
-            selectedIcon = painterResource(R.drawable.selected_my_network),
-            label = "My Network"
-        ),
-        BottomItem(
-            icon = painterResource(R.drawable.create),
-            selectedIcon = painterResource(R.drawable.create),
-            label = "Post"
-        ),
-        BottomItem(
-            icon = painterResource(R.drawable.notifications),
-            selectedIcon = painterResource(R.drawable.selected_notifications),
-            label = "Notifications"
-        ),
-        BottomItem(
-            icon = painterResource(R.drawable.jobs),
-            selectedIcon = painterResource(R.drawable.selected_jobs),
-            label = "Jobs"
-        )
-    )
-
-    var selectedItem by remember { mutableStateOf(navItems[0]) }
-
     AppScaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            CommonTopBar(
-                scrollBehavior = scrollBehavior,
-                showBack = false,
-                actions = {
-
-                    Icon(
-                        painter = painterResource(R.drawable.message),
-                        contentDescription = "Message",
-                        modifier = Modifier.size(24.dp),
-                        tint = MaterialTheme.colorScheme.tertiary
-                    )
-
-                },
-                avatar = {
-                    Box(
-                        modifier = Modifier
-                            .background(
-                                shape = CircleShape,
-                                color = colorResource(R.color.black)
-                            )
-                            .size(32.dp)
-                    ) {
-
-                    }
-                })
-        },
-        bottomBar = {
-            CommonBottomBar(
-                actions = {
-                    navItems.forEach { item ->
-                        CustomBarItem(item.copy(isSelected = item == selectedItem), onClick = {
-                            selectedItem = item
-                        })
-                    }
-                }
-            )
-        },
         content = { innerPadding ->
             HomeContent(
                 paddingValues = PaddingValues(
