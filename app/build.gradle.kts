8881862ace76ffd30846b46e8e937b9ffc2a8dea
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-10-30 04:01:05 +0530

    Add Datastore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,4 +88,7 @@ dependencies {
     // Coil
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
+
+    // Datastore
+    implementation(libs.androidx.datastore.preferences)
 }
