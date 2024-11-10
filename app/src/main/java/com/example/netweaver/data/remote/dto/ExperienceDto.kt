8a593748d39a5cf8dc37de37101566df51573038
Aarav Mahajan <aaravmahajan2003@gmail.com>
2024-11-10 06:36:24 +0530

    Update education model, experience dto

--- app/src/main/java/com/example/netweaver/data/remote/dto/ExperienceDto.kt
+++ app/src/main/java/com/example/netweaver/data/remote/dto/ExperienceDto.kt
@@ -1,40 +1,36 @@
 package com.example.netweaver.data.remote.dto
 
 import com.example.netweaver.domain.model.Experience
+import kotlinx.datetime.Clock.System.now
 import kotlinx.datetime.Instant
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ExperienceDto(
     @SerialName("id")
-    val id: String,
+    val id: String = "",
     @SerialName("user_id")
-    val userId: String,
+    val userId: String = "",
     @SerialName("company_name")
-    val companyName: String,
+    val companyName: String = "",
     @SerialName("position")
-    val position: String,
+    val position: String = "",
     @SerialName("start_date")
-    val startDate: Instant,
+    val startDate: Instant = now(),
     @SerialName("end_date")
-    val endDate: Instant,
+    val endDate: Instant = now(),
     @SerialName("created_at")
-    val createdAt: Instant,
+    val createdAt: Instant = now(),
     @SerialName("updated_at")
-    val updatedAt: Instant
+    val updatedAt: Instant = now()
 )
-
-fun Instant.toDateTime(): LocalDate = this.toLocalDateTime(TimeZone.currentSystemDefault()).date
 
 fun ExperienceDto.toDomain() = Experience(
     id = id,
     userId = userId,
     companyName = companyName,
     position = position,
-    startDate = startDate.toDateTime(),
-    endDate = endDate.toDateTime()
+    startDate = startDate,
+    endDate = endDate
 )
--- app/src/main/java/com/example/netweaver/domain/model/Experience.kt
+++ app/src/main/java/com/example/netweaver/domain/model/Experience.kt
@@ -1,12 +1,13 @@
 package com.example.netweaver.domain.model
 
-import kotlinx.datetime.LocalDate
+import kotlinx.datetime.Clock.System.now
+import kotlinx.datetime.Instant
 
 data class Experience(
     val id: String = "",
     val userId: String = "",
     val companyName: String = "",
     val position: String = "",
-    val startDate: LocalDate? = null,
-    val endDate: LocalDate? = null
+    val startDate: Instant = now(),
+    val endDate: Instant? = now()
 )
